151ec1725860dfd6bffb7d268c53c4a3231b2da2
John Heber Huallpa Cisneros <john.heber.huallpa.cisneros09@gmail.com>
2026-05-08 09:09:48 -0300

    Add files via upload

---
+++ JohnHuallpa-ACT11/11.1/Program11.1.cs
@@ -0,0 +1,110 @@
+﻿/*
+Se desea saber la temperatura media trimestral de cuatro paises. Para ello se tiene como
+dato las temperaturas medias mensuales de dichos paises.
+Se pide ingresar el nombre del país y seguidamente las tres temperaturas medias
+mensuales.
+Seleccionar las estructuras de datos adecuadas para el almacenamiento de los datos en
+memoria.
+a. Cargar por teclado los nombres de los paises y las temperaturas medias mensuales.
+b. Imprimir los nombres de las paises y las temperaturas medias mensuales de las
+mismas.
+c. Calcular la temperatura media trimestral de cada país.
+d. Imprimir los nombres de los paises y las temperaturas medias trimestrales.
+e. Imprimir el nombre del país con la temperatura media trimestral mayor. 
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11._1
+{
+     class Program
+    {
+        private string[] Paises;
+        private double[,] Climas;
+        private double[] TemperaturaTrimestral;
+        public void CargarM()
+        {
+            Climas = new double[4, 3];
+            Paises = new string [4];
+
+            for(int i = 0; i < Paises.Length; i++)
+            {
+                Console.WriteLine("Ingresa el nombre del pais: ");
+                Paises[i] = Console.ReadLine();
+
+                    for(int c = 0; c < Climas.GetLength(1); c++)
+                    {
+                        Console.WriteLine("Ingresa las 3 temperaturas medias mensuales de ese pais: ");
+                        String valor = Console.ReadLine();
+                        Climas[i, c] = double.Parse(valor);
+                    }
+            }
+        }
+        public void Imprimir()
+        {
+            Console.WriteLine("Pais: ");
+
+            for(int i = 0; i < Paises.Length; i++)
+            {
+                Console.WriteLine(Paises[i] + " ");
+                Console.WriteLine("Sus 3 temperaturas mensuales son: ");
+                for(int c = 0; c < Climas.GetLength(1); c++)
+                {
+                    Console.WriteLine(Climas[i, c]);
+                }
+            }
+        }
+        public void TTM()
+        {
+            TemperaturaTrimestral = new double[4];
+            for(int f = 0; f < Climas.GetLength(0); f++)
+            {
+                double suma = 0;
+                for(int c = 0; c < Climas.GetLength(1); c++)
+                {
+                    suma = suma + Climas[f, c];
+                }
+                TemperaturaTrimestral[f] = suma / Climas.GetLength(1);
+            }
+        }
+        public void ImprimirPromedioT()
+        {
+            Console.WriteLine("Paises con su promedio trimestral: ");
+
+            for(int f = 0; f < Climas.GetLength(0); f++)
+            {
+                Console.WriteLine(Paises[f] + ": " + TemperaturaTrimestral[f]);
+            }
+        }
+        public void PromedioMayor()
+        {
+            double Pmayor = TemperaturaTrimestral[0];
+            string PaisMayor = Paises[0];
+            for(int c = 0; c < TemperaturaTrimestral.Length; c++)
+            {
+                if (TemperaturaTrimestral[c] > Pmayor)
+                {
+                    Pmayor = TemperaturaTrimestral[c];
+                    PaisMayor = Paises[c];
+                }
+            }
+            Console.WriteLine("El pais con mayor temperatura trimestral es: " + PaisMayor);
+
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.CargarM();
+            p.Imprimir();
+            p.TTM();
+            p.ImprimirPromedioT();
+            p.PromedioMayor();
+
+        }
+    }
+}
---
+++ JohnHuallpa-ACT11/11.3/Program11.3.cs
@@ -0,0 +1,71 @@
+﻿/*
+Confeccionar una clase para administrar los días que han faltado los 3 empleados de una
+empresa.
+Definir un vector de 3 elementos de tipo string para cargar los nombres y una matriz
+irregular para cargar los días que han faltado cada empleado (cargar el número de día que
+faltó)
+Cada fila de la matriz representa los días de cada empleado.
+a. Mostrar los empleados con la cantidad de inasistencias.
+b. Cuál empleado faltó menos días.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11._3
+{
+    class Program
+    {
+        private string[] Empleados;
+        private int[][] DiasAusentes;
+        public void Carga()
+        {
+            Empleados = new string[3];
+            DiasAusentes = new int[3][];
+
+            for (int f = 0; f < Empleados.Length; f++)
+            {
+                Console.WriteLine("Ingresa el nombre del empleado: ");
+                Empleados[f] = Console.ReadLine();
+
+                Console.WriteLine("Cantidad de dias ausentes del empleado " + (f + 1) + ":");
+                int Columnas = int.Parse(Console.ReadLine());
+                DiasAusentes[f] = new int[Columnas];
+            }
+        }
+        public void Imprimir()
+        {
+             for(int f = 0; f < Empleados.Length; f++)
+            {
+                Console.WriteLine("El empleado " + Empleados[f] + " faltó " + DiasAusentes[f].Length + " dias");
+            }   
+        }
+        
+        public void ECMI()
+        {
+            int MenosFaltas = DiasAusentes[0].Length;
+            string EmpleadoTop = Empleados[0];
+
+            for(int f = 0; f < Empleados.Length; f++)
+            {
+                if (DiasAusentes[f].Length < MenosFaltas)
+                {
+                    EmpleadoTop = Empleados[f];
+                    MenosFaltas = DiasAusentes[f].Length;
+                }
+            }
+            Console.WriteLine("El empleado que falto menos dias fue: " + EmpleadoTop);
+
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Carga();
+            p.Imprimir();
+            p.ECMI();
+        }
+    }
+}
