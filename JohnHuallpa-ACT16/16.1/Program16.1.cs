4d3603d422ac6c12476ab8b2e31131ddf14094ad
John Heber Huallpa Cisneros <john.heber.huallpa.cisneros09@gmail.com>
2026-06-11 08:49:59 -0300

    Add files via upload

---
+++ JohnHuallpa-ACT16/16.1/Program16.1.cs
@@ -0,0 +1,96 @@
+﻿/*
+1. Confeccionar una clase Persona que tenga como atributos el nombre y la
+edad (definir las propiedades para poder acceder a dichos atributos). Definir
+un método para imprimirlos. Plantear una segunda clase Empleado que
+herede de la clase Persona. Añadir un atributo sueldo ( y su propiedad) y el
+método para imprimir su sueldo. Definir un objeto de la clase Persona y
+llamar a sus métodos y propiedades. También crear un objeto de la clase
+Empleado y llamar a sus métodos y propiedades.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16._1
+{
+    public class Persona
+    {
+        protected string nombre;
+        protected int edad;
+
+        public string Nombre
+        {
+
+            set
+            {
+                nombre = value;
+            }
+            get
+            {
+                return nombre;
+            }
+        }
+        public int Edad
+        {
+            set
+            {
+                edad = value;
+            }
+            get
+            {
+                return edad;
+            }
+        }
+        public void Imprimir()
+        {
+            Console.WriteLine($"Nombre: {nombre} edad: {edad}");
+        }
+    }
+    public class Empleado : Persona
+    {
+        protected int sueldo;
+        public int Sueldo
+        {
+            set
+            {
+                sueldo = value;
+            }
+            get
+            {
+                return sueldo;
+            }
+        }
+        public void ImprimirSueldo()
+        {
+            {
+                Console.WriteLine($"Sueldo del empleado ${sueldo}");
+            }
+            Console.ReadKey();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona p = new Persona();
+            p.Nombre = "Tapia";
+            p.Edad = 25;
+
+            Console.WriteLine("Datos de la persona: ");
+            p.Imprimir();
+            Console.WriteLine();
+
+            Empleado e = new Empleado();
+            e.Nombre = "Humongosaurio";
+            e.Edad = 33;
+            e.Sueldo = 150000;
+
+            Console.WriteLine("Datos de los empleados: ");
+            e.Imprimir();
+            e.ImprimirSueldo();
+        }
+    }
+}
---
+++ JohnHuallpa-ACT16/16.2/Program16.2.cs
@@ -0,0 +1,122 @@
+﻿/*
+2. Plantear una clase Producto y otra clase Inventario.
+La clase Producto debe tener como atributos privados el nombre, precio y
+stock. Definir propiedades para acceder a estos atributos, asegurando que el
+stock no pueda ser negativo y el precio sea mayor a cero.
+La clase Inventario debe contener 3 objetos de la clase Producto. Definir un
+método para mostrar todos los productos ordenados de menor a mayor en
+base al precio, además, mostrar el producto más caro y más barato del
+inventario.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16._2
+{
+    class Producto
+    {
+        private string nombre;
+        private double precio;
+        private int stock;
+
+        public string Nombre
+        {
+            set
+            {
+                nombre = value;
+            }
+            get
+            {
+                return nombre;
+            }
+        }
+
+        public double Precio
+        {
+            set
+            {
+                if (value > 0)
+                    precio = value;
+
+                else
+                    Console.WriteLine("El precio debe de ser mayor a 0");
+            }
+            get
+            {
+                return precio;
+            }
+        }
+
+        public int Stock
+        {
+            set
+            {
+                if (value >= 0)
+                    stock = value;
+
+                else
+                    Console.WriteLine("El stock no puede estar en negativo");
+            }
+            get
+            {
+                return stock;
+            }
+        }
+    }
+    class Inventario
+    {
+        private Producto[] productos = new Producto[3];
+
+        public Inventario()
+        {
+            productos[0] = new Producto();
+            productos[0].Nombre = "Leche";
+            productos[0].Precio = 5;
+            productos[0].Stock = 10;
+
+            productos[1] = new Producto();
+            productos[1].Nombre = "Cereal";
+            productos[1].Precio = 2;
+            productos[1].Stock = 1;
+
+            productos[2] = new Producto();
+            productos[2].Nombre = "cuchara";
+            productos[2].Precio = 6;
+            productos[2].Stock = 12;
+        }
+        public void ImprimirOrdenar()
+        {
+            for(int i = 0; i < productos.Length - 1; i++)
+            {
+                for (int j = 0; j < productos.Length; j++)
+                {
+                    if (productos[i].Precio > productos[i + 1].Precio)
+                    {
+                        double aux = productos[i].Precio;
+                        productos[i].Precio = productos[j].Precio;
+                        productos[j].Precio = aux;
+                    }
+                }
+            }
+            Console.WriteLine("Productos ordenados de menor a mayor en precios: ");
+
+            for (int i = 0; i < productos.Length; i++)
+            {
+                Console.WriteLine($"{productos[i].Nombre} con el precio de: ${productos[i].Precio} ");
+            }
+
+            Console.WriteLine($"El producto más barato es: {productos[0].Nombre}");
+            Console.WriteLine($"El producto más caro es: {productos[2].Nombre}");
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+            Inventario i = new Inventario();
+            i.ImprimirOrdenar();
+        }
+    }
+}
